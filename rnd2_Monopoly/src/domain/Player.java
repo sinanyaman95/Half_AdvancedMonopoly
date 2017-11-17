6a9ca38660e8a5ce7433bdfeed28fd295d5abfbe
Murat Doğan <mdogan13@ku.edu.tr>
2017-11-17 10:35:53 +0300

    Player constructor

--- rnd2_Monopoly/src/domain/Player.java
+++ rnd2_Monopoly/src/domain/Player.java
@@ -5,10 +5,15 @@ import java.util.ArrayList;
 public class Player {
 
 	private String name;
-	private int position;
+	private int position=0;
 	private double balance;
 	private ArrayList<Square> ownedProperties;
 	
 	
-	
+	public Player(String name, double balance) {
+		name=this.name;
+		balance=this.balance;
+		
+		
+	}
 }
