524590437d51b41c8e95079a8a249c94598dad4f
Murat Doğan <mdogan13@ku.edu.tr>
2017-11-17 10:32:57 +0300

    Square interface, Test and Player Classes

--- rnd2_Monopoly/src/domain/Player.java
+++ rnd2_Monopoly/src/domain/Player.java
@@ -1,10 +1,14 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class Player {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private String name;
+	private int position;
+	private double balance;
+	private ArrayList<Square> ownedProperties;
+	
+	
+	
 }
