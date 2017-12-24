9678c981a253def1c10303d817c173d2d15384f3
Murat Doğan <mdogan13@ku.edu.tr>
2017-12-24 20:43:21 +0300

    bot and package update

--- rnd2_Monopoly/src/gui/MonopolyBotGUI.java
+++ rnd2_Monopoly/src/gui/MonopolyBotGUI.java
@@ -1,35 +1,41 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.Timer;
 
 import domain.MonopolyGameController;
 
 public class MonopolyBotGUI {
-	JFrame botFrame = new JFrame();
-	JLabel botLabel = new JLabel("");
-	static ImageIcon neutral = new ImageIcon(
-			MonopolyBoard.class.getResource("/pics/Challenge_Accept_4fedc45a89cf3_large.jpeg"));
-	static ImageIcon bored = new ImageIcon(
-			MonopolyBoard.class.getResource("/pics/Annoyed_Dude_Dec_4fad9a77208a5_large.png"));
-	static ImageIcon amused = new ImageIcon(MonopolyBoard.class.getResource("/pics/lol.jpg"));
-	static ImageIcon mad = new ImageIcon(
-			MonopolyBoard.class.getResource("/pics/Mad_Rage_Face_Di_4fedc4e6aee20_large.jpeg"));
-	static ImageIcon afraid = new ImageIcon(MonopolyBoard.class.getResource("/pics/Raisins_Face.jpg"));
+	JPanel botPanel = new JPanel();
+	JLabel botLabel = new JLabel("",SwingConstants.CENTER);
+	static ImageIcon neutral = new ImageIcon(MonopolyBoard.class.getResource("/gui/pics/Challenge_Accept_4fedc45a89cf3_large.jpeg"));
+	static ImageIcon bored = new ImageIcon(MonopolyBoard.class.getResource("/gui/pics/Annoyed_Dude_Dec_4fad9a77208a5_large.png"));
+	static ImageIcon amused = new ImageIcon(MonopolyBoard.class.getResource("/gui/pics/lol.jpg"));
+	static ImageIcon mad = new ImageIcon(MonopolyBoard.class.getResource("/gui/pics/Mad_Rage_Face_Di_4fedc4e6aee20_large.jpeg"));
+	static ImageIcon afraid = new ImageIcon(MonopolyBoard.class.getResource("/gui/pics/Raisins_Face.jpg"));
 	Timer emotionTimer;
 	int updateTime = 1000;
 
 	MonopolyBotGUI() {
-		botFrame.setVisible(true);
-		botFrame.setBounds(200, 200, 520, 500);
-		botFrame.add(botLabel);
+		
+		botPanel.setLayout(new GridBagLayout());
+		botPanel.setBounds(200, 200, 520, 500);
+		botPanel.add(botLabel);
 		botLabel.setIcon(neutral);
-
+		botPanel.setBackground(Color.white);
+		//botLabel.setPreferredSize(new Dimension(520, 500));
+		botLabel.setMaximumSize(new Dimension(520, 500));
+		
 		ActionListener emoTask = new ActionListener() {
 
 			public void actionPerformed(ActionEvent evt) {
